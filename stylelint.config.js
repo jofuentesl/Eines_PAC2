ccc4e28a6dc9c2a455a9478fd563db5da7a0f0c7
jofuentesl <jofuentesle@uoc.edu>
2020-11-29 17:36:59 +0100

    update _home.scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -21,7 +21,8 @@ module.exports = {
             "screen",
             "use",
             "include",
-            "mixin"
+            "mixin",
+            "extend"
           ]
         }
       ]
